603dd5176c8863883c7a36bc7c2308843a4b8902
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-09-09 06:19:59 -0500

    logra ver password

---
+++ resources/js/components/Password.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input :type="inputField" :id="id" :name="id" class="form-control form-input form-input-bordered w-full" required />
+    <button type="button" @click="switchVisibilityPassword">Ver clave</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputField: "password",
+    };
+  },
+  props: {
+    id: {
+      type: String,
+      default: "password",
+    },
+  },
+  methods: {
+    switchVisibilityPassword() {
+      this.inputField = this.inputField == "password" ? "text" : "password";
+    },
+  },
+  mounted() {
+    console.log("Hola");
+  },
+};
+</script>
+
+<style>
+</style>
