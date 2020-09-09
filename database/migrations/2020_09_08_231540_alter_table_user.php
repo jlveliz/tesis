26c4ea3fa25851376405907606c59ff1dd252ceb
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-09-09 04:58:28 -0500

    add nova to project

---
+++ database/migrations/2020_09_08_231540_alter_table_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('lastname')->nullable()->after('name');
+                $table->string('num_identification')->nullable()->unique()->after('lastname');
+                $table->boolean('allow_login')->default(1)->after('remember_token');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn(['lastname','num_identification','allow_login']);
+            });
+        }
+    }
+}
